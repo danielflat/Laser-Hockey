19c98eb35fa8b10bb44337c8e1a33ca28b6e1215
Flat <daniel.flat@wps.de>
2025-01-17 09:51:48 +0100

    df: finally cluster ready. I dont know why it took so long

--- src/util/directoryutil.py
+++ src/util/directoryutil.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import torch
 
 
 def get_repo_path():
@@ -10,13 +11,19 @@ def get_repo_path():
         repo_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
         return repo_path
     except subprocess.CalledProcessError:
-        return "/mnt/beegfs/home/stud204/Project_RL"  # not a good style but it has to work on the cluster
-        # return None
+        return None
 
 
 def get_path(directory):
     """
     Here you can construct the path for a resource with respect to the repository main directory.
     """
-    repo_path = get_repo_path()
+
+    # df: Lazy function to not change the path on your own manually, just to make it work on the cluster.
+    # Because on the cluster `get_repo_path` does not work. Therefore, we have to get hacky.
+    # Easiest way: I only have a cpu, the cluster uses a gpu. Simple solution! 🙈
+    if torch.cuda.is_available():
+        repo_path = "/home/stud204/Project_RL"  # if you want to use the cluster
+    else:
+        repo_path = get_repo_path()  # if you want to use it locally
     return os.path.join(repo_path, directory)
