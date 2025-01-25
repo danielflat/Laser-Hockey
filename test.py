72d77f60cbea77b8c7906ba0820c4fbe5d16ada4
Flat <daniel.flat@wps.de>
2025-01-25 12:53:16 +0100

    df: extended the test class for better testing

--- test.py
+++ test.py
@@ -9,81 +9,80 @@ import torch
 
 from src.agent import Agent
 from src.settings import AGENT_SETTINGS, DQN_SETTINGS, PPO_SETTINGS, MPO_SETTINGS, TD_MPC2_SETTINGS
-from src.util.constants import DDPG_ALGO, DQN_ALGO, HOCKEY, MPO_ALGO, HUMAN, PENDULUM, HALFCHEETAH, TDMPC2_ALGO
+from src.util.constants import DDPG_ALGO, DQN_ALGO, HOCKEY, MPO_ALGO, HUMAN, PENDULUM, HALFCHEETAH, RANDOM_ALGO, \
+    STRONG_COMP_ALGO, \
+    TDMPC2_ALGO
 from src.util.contract import initAgent, initEnv, initSeed
 from src.util.directoryutil import get_path
 
 import hockey.hockey_env as h_env
 
 """
-This is a test class with which you can sanity check your agent's performances.
+This is a test class with which you can sanity check your agent's checkpoints against other agents, e.g. against a human agent, a strong agent 
+or against another algorithmic agent.
 The pendulum and the hockey environment are supported.
 You can go and adjust the settings below to test your agent's performance.
+
+In the following, we will document the settings variables in order to use this test class.
+
+Since the pendulum is a one player game, you only need to set the settings for player 1.
+For Hockey, you have to set variables for both players.
+
+With `TEST_CHECK_POINT_NAME_PLAYER_*`, you can set the path of the checkpoint '.pth' file in order to load the model for this agent.
+An example for this can be:
+TEST_CHECK_POINT_NAME_PLAYER_1 = get_path("good_checkpoints/hockey_tdmpc2_bad_25-01-22 15_05_13_000000060.pth")
+
+The same can be done for player 2. It is also possible to set the same checkpoint for both players the same way.
+
+`TEST_USE_ENV` is either HOCHEY or PENDULUM to set the test environment.
+
+`TEST_USE_ALGO_PLAYER_*` sets the algorithm for this player. It can be either "human" or the name for the algorithm. For that, check out constants.py `SUPPORTED_ALGORITHMS`
+
+TEST_NUMBER_DISCRETE_ACTIONS is deprecated now. But it can be used to discretize the continous action space into n bins.
+
+TEST_RENDER_MODE = HUMAN  # For whom do you want to render? None or HUMAN
+
+TEST_DEVICE, TEST_SEED and TEST_ITERATIONS should be clear to understand.
 
 Author: Daniel Flat
 """
 
-# Example usage of these setting parameters:
-# Example 01: Hockey
-# TEST_CHECK_POINT_NAME = get_path("good_checkpoints/hockey_ddpg_25-01-22 17_36_56_05000.pth")
-# TEST_USE_ENV = HOCKEY
-# TEST_USE_ALGO = DDPG_ALGO
-# TEST_MODE = "human" # "human", "strong" or "self"
-# TEST_NUMBER_DISCRETE_ACTIONS = None
-# TEST_SEED = 5
-# TEST_RENDER_MODE = HUMAN  # None or HUMAN
-# TEST_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# TEST_ITERATIONS = 10
-
-# EXAMPLE 02: Pendulum
-# TEST_CHECK_POINT_NAME = get_path("output/checkpoints/25-01-21 18_37_28/25-01-21 18_37_28_00100.pth")
-# TEST_USE_ENV = PENDULUM
-# TEST_USE_ALGO = DDPG_ALGO
-# TEST_MODE = "human" # "human", "strong" or "self"
-# TEST_NUMBER_DISCRETE_ACTIONS = None
-# TEST_SEED = 5
-# TEST_RENDER_MODE = HUMAN  # None or HUMAN
-# TEST_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# TEST_ITERATIONS = 10
-
-# output/checkpoints/25-01-21 18_37_28/25-01-21 18_37_28_00100.pth pendulum with DDPG
-# output/checkpoints/25-01-22 12_04_21/25-01-22 12_04_21_000000280.pth pendulum with TDMPC2
-
-# USEFUL CONSTANTS
-TEST_CHECK_POINT_NAME = get_path(
-    "output/checkpoints/25-01-22 12_04_21/25-01-22 12_04_21_000000280.pth")  # Which checkpoint do you want to test
-TEST_USE_ENV = PENDULUM  # On which environment do you want to test?
-TEST_USE_ALGO = TDMPC2_ALGO  # Which algorithm do you want to test?
-TEST_MODE = "human"  # HOCKEY ONLY: Against which agent do you want to play? "human", "strong" or "self" are supported
+# Settings for this class
+TEST_CHECK_POINT_NAME_PLAYER_1 = get_path(
+    "good_checkpoints/hockey_tdmpc2_bad_25-01-22 15_05_13_000000060.pth")  # Which checkpoint do you want to test
+TEST_CHECK_POINT_NAME_PLAYER_2 = get_path(
+    "good_checkpoints/hockey_tdmpc2_bad_25-01-22 15_05_13_000000060.pth")  # Which checkpoint do you want to test
+TEST_USE_ENV = HOCKEY  # On which environment do you want to test?
+TEST_USE_ALGO_PLAYER_1 = TDMPC2_ALGO  # Which algorithm do you want to test? Can be "human" or an algo constant
+TEST_USE_ALGO_PLAYER_2 = RANDOM_ALGO  # Only Hockey: Which algorithm do you want to test for player 2? Can be "human" or an algo constant
 TEST_NUMBER_DISCRETE_ACTIONS = None  # if you want to use discrete actions or continuous. If > 0, you use the DiscreteActionWrapper
 TEST_SEED = 100000  # Set a test seed if you want to
 TEST_RENDER_MODE = HUMAN  # For whom do you want to render? None or HUMAN
 TEST_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # which device are you using?
 TEST_ITERATIONS = 10  # The number of test iterations
 
-if __name__ == '__main__':
+
+def test():
     initSeed(seed = TEST_SEED, device = TEST_DEVICE)
     print(f"Test seed: {TEST_SEED}, Test Device: {TEST_DEVICE}")
 
     env = initEnv(TEST_USE_ENV, TEST_RENDER_MODE, TEST_NUMBER_DISCRETE_ACTIONS)
 
-    agent = initAgent(use_algo = TEST_USE_ALGO, env = env, agent_settings = AGENT_SETTINGS, device = TEST_DEVICE,
-                      checkpoint_name = TEST_CHECK_POINT_NAME)
-    agent.setMode(eval = True)
-
-    if TEST_USE_ENV == HOCKEY and TEST_MODE == "human":
+    if TEST_USE_ALGO_PLAYER_1 == "human":
         player1 = h_env.HumanOpponent(env = env, player = 1)
-        player2 = agent
-    elif TEST_USE_ENV == HOCKEY and TEST_MODE == "strong":
-        player1 = agent
-        player2 = h_env.BasicOpponent()
-    elif TEST_USE_ENV == HOCKEY and TEST_MODE == "self":
-        player1 = agent
-        player2 = copy.deepcopy(agent)
-    elif TEST_USE_ENV == PENDULUM:
-        player1 = agent
     else:
-        raise NotImplementedError
+        player1 = initAgent(use_algo = TEST_USE_ALGO_PLAYER_1, env = env, agent_settings = AGENT_SETTINGS,
+                            device = TEST_DEVICE,
+                            checkpoint_name = TEST_CHECK_POINT_NAME_PLAYER_1)
+        player1.setMode(eval = True)
+
+    if TEST_USE_ALGO_PLAYER_2 == "human":
+        player2 = h_env.HumanOpponent(env = env, player = 2)
+    else:
+        player2 = initAgent(use_algo = TEST_USE_ALGO_PLAYER_2, env = env, agent_settings = AGENT_SETTINGS,
+                            device = TEST_DEVICE,
+                            checkpoint_name = TEST_CHECK_POINT_NAME_PLAYER_2)
+        player2.setMode(eval = True)
 
     episode_steps = []
     episode_rewards = []
@@ -92,18 +91,27 @@ if __name__ == '__main__':
         total_steps = 0
         total_reward = 0
 
+        if isinstance(player1, Agent):
+            player1.reset()
+        if isinstance(player2, Agent):
+            player2.reset()
+
         state, info = env.reset(seed = TEST_SEED + i_test)
         env.render()
         if TEST_USE_ENV == HOCKEY:
             state2 = env.obs_agent_two()
 
         for _ in count():
+            if TEST_USE_ENV == HOCKEY:
+                env.render()
+
             if isinstance(player1, Agent):
                 state = torch.tensor(state, device = TEST_DEVICE, dtype = torch.float32)
 
+            action1 = player1.act(state)
             if TEST_USE_ENV == HOCKEY and isinstance(player2, Agent):
                 state2 = torch.tensor(state2, device = TEST_DEVICE, dtype = torch.float32)
-            action1 = player1.act(state)
+
             if TEST_USE_ENV == HOCKEY:
                 action2 = player2.act(state2)
                 next_step, reward, terminated, truncated, info = env.step(np.hstack([action1, action2]))
@@ -128,3 +136,7 @@ if __name__ == '__main__':
     print(f"Tests done! "
           f"Durations average: {np.array(episode_steps).mean():.4f} | Durations std. dev: {np.array(episode_steps).std():.4f} | Durations variance: {np.array(episode_steps).var():.4f} | "
           f"Reward average: {np.array(episode_rewards).mean():.4f} | Reward std. dev: {np.array(episode_rewards).std():.4f} | Reward variance: {np.array(episode_rewards).var():.4f}")
+
+
+if __name__ == '__main__':
+    test()
